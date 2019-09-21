9a1af2f7bdef6a3a568a423901499d9925c54ded
rob <rob@pigatron-industries.co.uk>
2019-09-21 18:54:22 +0100

    Set port name in config

--- seq/main.py
+++ seq/main.py
@@ -1,6 +1,15 @@
-from bootstrap import *
+#from bootstrap import *
 import sys
 import mido
+import configparser
+import sequencer
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+seq = sequencer.Sequencer(config)
+
+
 
 # Console command to play a file
 def play():
--- seq/sequencer.py
+++ seq/sequencer.py
@@ -6,8 +6,16 @@ class Sequencer():
     def __init__(self, config):
         print("Sequencer::init")
         self.config = config
-        self.outport = mido.open_output(name='foo', virtual=toBoolean(config['Output']['virtual']))
+        try:
+            self.outport = mido.open_output(name=config['Output']['port'], virtual=toBoolean(config['Output']['virtual']))
+        except OSError as err:
+            print("Error creating outport: {0}".format(err))
+            print("Please configure port name in config.ini, possible port names are:")
+            print(mido.get_output_names())
+            raise
 
-    def noteOn(msg):
+
+    def noteOn(self, msg):
+        print("noteOn")
         print(msg.bytes())
-        outport.send(msg)
+        self.outport.send(msg)
