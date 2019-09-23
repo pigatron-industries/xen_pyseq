bd24aac8a1189bc1c3e052c2b3fd167a2a65f8d2
Robert Ellis <robert.ellis@rightmove.co.uk>
2019-09-23 17:32:37 +0100

    Changed test score to test note off note on and note functions

--- scores/test.py
+++ scores/test.py
@@ -8,25 +8,32 @@ channel = 0
 setBpm(120)
 setNoteLength(L8)
 
+noteOn(channel, 60)
+noteOn(channel, 62)
+noteOn(channel, 64)
+noteOn(channel, 65)
+
+wait(L4)
+
+noteOff(channel, 60)
+noteOff(channel, 62)
+noteOff(channel, 64)
+noteOff(channel, 65)
+
+wait(L4)
+
 note(channel, 60)
 note(channel, 62)
 note(channel, 64)
 note(channel, 65)
+
 wait(L4)
+
 note(channel, 62)
 note(channel, 64)
 note(channel, 66)
 note(channel, 68)
-wait(L4)
-note(channel, 60)
-note(channel, 62)
-note(channel, 64)
-note(channel, 65)
-wait(L4)
-note(channel, 62)
-note(channel, 64)
-note(channel, 66)
-note(channel, 68)
+
 wait(L4)
 
 
