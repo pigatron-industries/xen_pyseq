19e4ce38a846fe9197f4d37a18b865dd9f217a67
rob <rob@pigatron-industries.co.uk>
2019-10-26 15:22:49 +0100

    Added protobuf object for configuring midi2cv channels and sending over midi sysex

--- scores/test_sysex.py
+++ scores/test_sysex.py
@@ -1,4 +1,15 @@
+import protobuf.ChannelConfig_pb2 as channelConfig
 
+config = channelConfig.ChannelConfig()
+channelMapping = config.channelMapping.add()
+channelMapping.midiChannel = 1
+channelMapping.cvChannelFrom = 1
+channelMapping.cvChannelTo = 1
+channelMapping = config.channelMapping.add()
+channelMapping.midiChannel = 2
+channelMapping.cvChannelFrom = 2
+channelMapping.cvChannelTo = 2
+logging.info(config.SerializeToString())
 
-sysex(bytearray([1, 2, 3]))
+sysex(config.SerializeToString())
 wait(L4)
