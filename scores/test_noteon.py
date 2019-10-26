ca897c7e79c84cba68a0c8bfc791fa48d7c59772
rob <rob@pigatron-industries.co.uk>
2019-10-26 11:40:29 +0100

    changed test scores

--- scores/test_noteon.py
+++ scores/test_noteon.py
@@ -8,32 +8,11 @@ setNoteLength(L8)
 setChannel(0, 1)
 
 noteOn(60)
-noteOn(62)
-noteOn(64)
-noteOn(65)
 
 wait(L4)
 
 noteOff(60)
-noteOff(62)
-noteOff(64)
-noteOff(65)
 
-wait(L4)
-
-note(60)
-note(62)
-note(64)
-note(65)
-
-wait(L4)
-
-note(62)
-note(64)
-note(66)
-note(68)
-
-wait(L4)
 
 
 print("======================")
