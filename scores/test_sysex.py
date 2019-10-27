9b6555e3625a0a4ef9d8d9ea1635132bd574ddc4
rob <rob@pigatron-industries.co.uk>
2019-10-27 10:22:24 +0000

    changed sysex test

--- scores/test_sysex.py
+++ scores/test_sysex.py
@@ -2,13 +2,9 @@ import protobuf.ConfigMessage_pb2 as configMessage
 
 message = configMessage.ConfigWrapper()
 channelMapping = message.channelConfig.channelMapping.add()
-channelMapping.midiChannel = 1
-channelMapping.cvChannelFrom = 1
+channelMapping.midiChannel = 0
+channelMapping.cvChannelFrom = 0
 channelMapping.cvChannelTo = 1
-channelMapping = message.channelConfig.channelMapping.add()
-channelMapping.midiChannel = 2
-channelMapping.cvChannelFrom = 2
-channelMapping.cvChannelTo = 2
 logging.info(message.SerializeToString())
 
 sysex(message.SerializeToString())
